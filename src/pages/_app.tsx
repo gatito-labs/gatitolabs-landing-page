bec2b0d61df898b6547569dd5ac6cce8c6be50c8
tof <tof@pop-os.localdomain>
2022-06-01 11:15:48 -0400

    asfd+

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { FC, useEffect } from 'react';
+
 import { AppProps } from 'next/app';
+import TagManager from 'react-gtm-module';
 
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
-
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MS25GPB' });
+  }, []);
+  return <Component {...pageProps} />;
+};
 export default MyApp;
